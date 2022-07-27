5b4a06a77c1d1e698aa26dd6834fef379c26ec66
rutuja_mali <rutujamali111@gmail.com>
2022-07-28 02:57:57 +0530

    Monthly Employee wage

--- EmpTest.java
+++ EmpTest.java
@@ -4,8 +4,8 @@ public class EmpTest {
 	public static void main(String[] args) {
 		Employee emp=new Employee();
 		emp.Presenty();
-		int EmpWage=emp.DailyEmployeeWage();
-		System.out.println("Daily Employee wage is: "+EmpWage);
+		int EmpWage=emp.TotalEmpWage();
+		System.out.println("total Employee wage is: "+EmpWage);
 
 
 	}
--- Employee.java
+++ Employee.java
@@ -1,5 +1,6 @@
 
 public class Employee {
+	int NUM_OF_WORKING_DAYS = 0;
 	final int IS_PART_TIME = 1;
 	final int IS_FULL_TIME=2;
 	int EMP_RATE_PER_HOUR=20;
@@ -17,20 +18,33 @@ public class Employee {
 	{
 		int empHrs=0;
 		int empWage=0;
+		
 		int empCheck=(int) Math.floor(Math.random()*10)%3;
-		switch(empCheck)
-		{
-		case IS_PART_TIME:
-			empHrs=4;
-			break;
-		case IS_FULL_TIME:
-			empHrs=8;
-			break;
-		default:
-			empHrs=0;
-		}
-		empWage=empHrs*EMP_RATE_PER_HOUR;
+		
+			switch(empCheck)
+			{
+				case IS_PART_TIME:
+					empHrs=4;
+					break;
+				case IS_FULL_TIME:
+					empHrs=8;
+					break;
+				default:
+					empHrs=0;
+			}
+			empWage=empHrs*EMP_RATE_PER_HOUR;
+			
 		return empWage;
+	
 	}
-
+	public int TotalEmpWage()
+	{
+		int totalEmpWage=0;
+		for(int day=0;day<NUM_OF_WORKING_DAYS;day++)
+		{
+			totalEmpWage+=DailyEmployeeWage();
+		}
+		
+		return totalEmpWage;
+	}
 }
