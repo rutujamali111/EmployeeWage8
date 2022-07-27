f10462851681055c6decd49726a2c9413b75688e
rutuja_mali <rutujamali111@gmail.com>
2022-07-28 02:44:22 +0530

    Daily Employee wage

--- EmpTest.java
+++ EmpTest.java
@@ -2,9 +2,11 @@
 public class EmpTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Employee emp1=new Employee();
-		emp1.Presenty();
+		Employee emp=new Employee();
+		emp.Presenty();
+		int EmpWage=emp.DailyEmployeeWage();
+		System.out.println("Daily Employee wage is: "+EmpWage);
+
 
 	}
 
--- Employee.java
+++ Employee.java
@@ -1,13 +1,28 @@
 
 public class Employee {
+	int IS_FULL_TIME=1;
+	int EMP_RATE_PER_HOUR=20;
 	public void Presenty()
 	{
-		int IS_FULL_TIME=1;
+		
 		double empCheck=(Math.random()*10)%2;
 		if(empCheck==IS_FULL_TIME)
 			System.out.println("Employee is present..");
 		else
 			System.out.println("Employee is Absent");
+	}
+	
+	public int DailyEmployeeWage()
+	{
+		int empHrs=0;
+		int empWage=0;
+		double empCheck=Math.floor(Math.random()*10)%2;
+		if(empCheck==IS_FULL_TIME)
+			empHrs=8;
+		else
+			empHrs=0;
+		empWage=empHrs*EMP_RATE_PER_HOUR;
+		return empWage;
 	}
 
 }
