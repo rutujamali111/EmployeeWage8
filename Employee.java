75b2225e8442e5533d25b1aea88a071cca97ed2d
rutuja_mali <rutujamali111@gmail.com>
2022-07-28 02:47:18 +0530

    Daily Employee wage with part time

--- Employee.java
+++ Employee.java
@@ -1,11 +1,12 @@
 
 public class Employee {
-	int IS_FULL_TIME=1;
+	int IS_PART_TIME = 1;
+	int IS_FULL_TIME=2;
 	int EMP_RATE_PER_HOUR=20;
 	public void Presenty()
 	{
 		
-		double empCheck=(Math.random()*10)%2;
+		double empCheck=(Math.random()*10)%3;
 		if(empCheck==IS_FULL_TIME)
 			System.out.println("Employee is present..");
 		else
@@ -17,7 +18,9 @@ public class Employee {
 		int empHrs=0;
 		int empWage=0;
 		double empCheck=Math.floor(Math.random()*10)%2;
-		if(empCheck==IS_FULL_TIME)
+		if (empCheck==IS_PART_TIME)
+			empHrs=4;
+		else if(empCheck==IS_FULL_TIME)
 			empHrs=8;
 		else
 			empHrs=0;
